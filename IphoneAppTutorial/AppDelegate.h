af0a562f29747cdd6686af39b3f4c9b359007689
Sugat Nagavkar <sugatnagavkar@sugats-mbp.mshome.net>
2014-11-20 22:40:35 +0530

    Initial Commit

---
+++ IphoneAppTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IphoneAppTutorial
+//
+//  Created by Sugat Nagavkar on 20/11/14.
+//  Copyright (c) 2014 Sugat Nagavkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IphoneAppTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IphoneAppTutorial
+//
+//  Created by Sugat Nagavkar on 20/11/14.
+//  Copyright (c) 2014 Sugat Nagavkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
